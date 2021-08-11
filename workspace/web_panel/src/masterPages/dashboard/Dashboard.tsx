046f148587aac9de813d9c8d0fffba4bf919e872
amir.ashrafi <dipkastel@gmail.com>
2021-08-11 17:49:21 +0430

    add some front dashboard to choose

--- workspace/web_panel/src/masterPages/dashboard/Dashboard.tsx
+++ workspace/web_panel/src/masterPages/dashboard/Dashboard.tsx
@@ -6,13 +6,15 @@ import UserList from "../../pages/userList/UserList";
 import {BrowserRouter as Router, Switch, Route,Redirect} from "react-router-dom";
 import User from "../../pages/user/User";
 import "./Dashboard.css"
+import Places from "../../pages/places/Places";
 
 export default function Dashboard({onLogout}:any){
     return (
         <Router>
             <Topbar onLogout={onLogout}/>
             <div className="main-container">
-                <Sidebar/>
+                <div className="side-menu"><Sidebar/></div>
+                <div className="page">
                 <Switch>
                     <Route exact path="/">
                         <Home/>
@@ -23,7 +25,11 @@ export default function Dashboard({onLogout}:any){
                     <Route path="/user/:userId">
                         <User/>
                     </Route>
+                    <Route path="/places">
+                        <Places/>
+                    </Route>
                 </Switch>
+                </div>
             </div>
         </Router>
     )
--- workspace/web_panel/src/pages/userList/UserList.tsx
+++ workspace/web_panel/src/pages/userList/UserList.tsx
@@ -6,96 +6,101 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export default function UserList() {
-  const [data, setData] = useState(userRows);
+    const [data, setData] = useState(userRows);
 
-  const handleDelete = (id:number) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-  
-  const columns = [
-    { field: "id", headerName: "ID", width: 100 ,
-      renderCell: (params:any) => {
-        return (
-            <div>
-              {params.row.id}
-            </div>
-        );
-      } },
-    {
-      field: "user",
-      headerName: "User",
-      width: 150,
-      renderCell: (params:any) => {
-        return (
-          <div className="userListUser">
-            <img className="userListImg" src={params.row.avatar} alt="" />
-            {params.row.username}
-          </div>
-        );
-      },
-    },
-    { field: "email", headerName: "Email", width: 200 ,
-    renderCell: (params:any) => {
+    const handleDelete = (id:number) => {
+        setData(data.filter((item) => item.id !== id));
+    };
+
+    const columns = [
+        { field: "id", headerName: "ID", width: 100 ,
+            renderCell: (params:any) => {
+                return (
+                    <div>
+                        {params.row.id}
+                    </div>
+                );
+            } },
+        {
+            field: "user",
+            headerName: "User",
+            width: 150,
+            renderCell: (params:any) => {
+                return (
+                    <div className="userListUser">
+                        <img className="userListImg" src={params.row.avatar} alt="" />
+                        {params.row.username}
+                    </div>
+                );
+            },
+        },
+        { field: "email", headerName: "Email", width: 200 ,
+            renderCell: (params:any) => {
+                return (
+                    <div>
+                        {params.row.email}
+                    </div>
+                );
+            }},
+        {
+            field: "status",
+            headerName: "Status",
+            width: 120,
+            renderCell: (params:any) => {
+                return (
+                    <div>
+                        {params.row.status}
+                    </div>
+                );
+            }
+        },
+        {
+            field: "transaction",
+            headerName: "Transaction Volume",
+            width: 190,
+            renderCell: (params:any) => {
+                return (
+                    <div>
+                        {params.row.transaction}
+                    </div>
+                );
+            }
+
+        },
+        {
+            field: "action",
+            headerName: "Action",
+            width: 150,
+            renderCell: (params:any) => {
+                return (
+                    <>
+                        <Link to={"/user/" + params.row.id}>
+                            <button className="userListEdit">Edit</button>
+                        </Link>
+                        <DeleteOutline
+                            className="userListDelete"
+                            onClick={() => handleDelete(params.row.id)}
+                        />
+                    </>
+                );
+            },
+        },
+    ];
+
+    console.log(data);
     return (
-        <div>
-          {params.row.email}
+        <div className="userList">
+            <div style={{ display: 'flex', height: '650px' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid
+                        rows={data}
+                        disableSelectionOnClick
+                        columns={columns}
+                        pageSize={10}
+                        checkboxSelection
+                    />
+                </div>
+            </div>
         </div>
     );
-  }},
-    {
-      field: "status",
-      headerName: "Status",
-      width: 120,
-    renderCell: (params:any) => {
-    return (
-        <div>
-          {params.row.status}
-        </div>
-    );
-  }
-    },
-    {
-      field: "transaction",
-      headerName: "Transaction Volume",
-      width: 190,
-      renderCell: (params:any) => {
-        return (
-            <div>
-              {params.row.transaction}
-            </div>
-        );
-      }
-
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 150,
-      renderCell: (params:any) => {
-        return (
-          <>
-            <Link to={"/user/" + params.row.id}>
-              <button className="userListEdit">Edit</button>
-            </Link>
-            <DeleteOutline
-              className="userListDelete"
-              onClick={() => handleDelete(params.row.id)}
-            />
-          </>
-        );
-      },
-    },
-  ];
-
-  return (
-    <div className="userList">
-      <DataGrid
-        rows={data}
-        disableSelectionOnClick
-        columns={columns}
-        pageSize={12}
-        checkboxSelection
-      />
-    </div>
-  );
 }
