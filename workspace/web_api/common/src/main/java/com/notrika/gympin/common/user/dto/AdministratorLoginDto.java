5efaa07fc4e6bdda4b846f1c04b778afe40e9f5f
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-09-02 14:10:51 +0430

    placeClient add show

--- workspace/web_api/common/src/main/java/com/notrika/gympin/common/user/dto/AdministratorLoginDto.java
+++ workspace/web_api/common/src/main/java/com/notrika/gympin/common/user/dto/AdministratorLoginDto.java
@@ -6,12 +6,19 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class AdministratorLoginDto extends BaseDto<AdministratorLoginDto> {
+public class AdministratorLoginDto extends BaseDto<AdministratorLoginDto> /*implements UserDetails*/ {
 
     private AdminRoles administratorRoles = AdminRoles.ADMIN;
 
@@ -23,4 +30,40 @@ public class AdministratorLoginDto extends BaseDto<AdministratorLoginDto> {
 
     private String token;
 
+    //@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(administratorRoles.name()));
+        return authorities;
+    }
+
+    //@Override
+    public String getPassword() {
+        return token;
+    }
+
+    //@Override
+    public String getUsername() {
+        return phoneNumber;
+    }
+
+    //@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //@Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //@Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //@Override
+    public boolean isEnabled() {
+        return !isDeleted();
+    }
 }
