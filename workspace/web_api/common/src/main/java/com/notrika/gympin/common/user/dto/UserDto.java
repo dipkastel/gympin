e5e1698d4485646660d96d25ca8a03df6353ac9c
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-09-02 14:12:17 +0430

    placeClient add show

--- workspace/web_api/common/src/main/java/com/notrika/gympin/common/user/dto/UserDto.java
+++ workspace/web_api/common/src/main/java/com/notrika/gympin/common/user/dto/UserDto.java
@@ -6,15 +6,53 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class UserDto extends BaseDto<UserDto> {
+public class UserDto extends BaseDto<UserDto> /*implements UserDetails*/ {
     private UserRoles role = UserRoles.USER;
     private String username;
     private String phoneNumber;
     private String token;
 
+    //@Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.name()));
+        return authorities;
+    }
+
+    //@Override
+    public String getPassword() {
+        return token;
+    }
+
+    //@Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //@Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //@Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //@Override
+    public boolean isEnabled() {
+        return !this.isDeleted();
+    }
 }
