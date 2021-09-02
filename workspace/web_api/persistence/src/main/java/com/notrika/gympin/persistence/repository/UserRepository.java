7b32c3d582f189d4feedfc0442111f56254a36f3
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-09-02 14:13:16 +0430

    placeClient add show

--- workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/UserRepository.java
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.notrika.gympin.persistence.repository;
 
+import com.notrika.gympin.dao.location.Place;
 import com.notrika.gympin.dao.user.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +15,9 @@ public interface UserRepository extends BaseRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByPhoneNumber(String phoneNumber);
+
+    //@Query("select u from User u,PlaceOwner po where u.id=po.user.id and po.place.id=:#{#place.id}")
+    List<User> getOwnersPlace(Place place);
+
 
 }
