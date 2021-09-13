c5624b47d006188477594541357b33e7340d29a0
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-09-14 03:08:26 +0430

    fix user

--- workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/SportRepository.java
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/SportRepository.java
@@ -4,5 +4,5 @@ import com.notrika.gympin.dao.sport.Sport;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SportRepository extends BaseRepository<Sport,Long> {
+public interface SportRepository extends BaseRepository<Sport, Long> {
 }
