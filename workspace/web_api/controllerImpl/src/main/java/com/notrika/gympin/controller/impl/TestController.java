835675dfd0a26fde675f7c3ccd1ad7f5ac373374
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-10-02 00:42:51 +0330

    [bug] fix tomcat problem

---
+++ workspace/web_api/controllerImpl/src/main/java/com/notrika/gympin/controller/impl/TestController.java
@@ -0,0 +1,18 @@
+package com.notrika.gympin.controller.impl;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<String>("Hello", HttpStatus.OK);
+    }
+
+}
--- workspace/web_api/framework/src/main/java/com/notrika/gympin/framework/spring/WebApiApplication.java
+++ workspace/web_api/framework/src/main/java/com/notrika/gympin/framework/spring/WebApiApplication.java
@@ -4,12 +4,13 @@ import com.notrika.gympin.persistence.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.notrika.gympin.*"})
 @EnableJpaRepositories(basePackages = {"com.notrika.gympin.persistence.*"}, repositoryBaseClass = BaseRepositoryImpl.class)
 @EntityScan("com.notrika.gympin.dao.*")
-public class WebApiApplication {
+public class WebApiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WebApiApplication.class, args);
     }
