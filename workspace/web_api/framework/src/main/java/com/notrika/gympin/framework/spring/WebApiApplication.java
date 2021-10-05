f27159768e6155184e14317ffd9fcf72d731921d
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-10-05 19:10:56 +0330

    [bug] fix standalone on tomcat

--- workspace/web_api/framework/src/main/java/com/notrika/gympin/framework/spring/WebApiApplication.java
+++ workspace/web_api/framework/src/main/java/com/notrika/gympin/framework/spring/WebApiApplication.java
@@ -4,6 +4,7 @@ import com.notrika.gympin.persistence.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -14,4 +15,11 @@ public class WebApiApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(WebApiApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(WebApiApplication.class); //super.configure(builder);
+    }
+
+
 }
