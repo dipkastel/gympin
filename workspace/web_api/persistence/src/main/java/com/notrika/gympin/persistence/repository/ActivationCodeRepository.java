32bb922178a5941ea9724786f3e0775a2bc40e80
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-10-19 22:23:37 +0330

    [bug] activation code expiration after retry

--- workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/ActivationCodeRepository.java
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/repository/ActivationCodeRepository.java
@@ -1,9 +1,11 @@
 package com.notrika.gympin.persistence.repository;
 
 import com.notrika.gympin.dao.activationCode.ActivationCode;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,7 +14,9 @@ public interface ActivationCodeRepository extends BaseRepository<ActivationCode,
 
     Optional<List<ActivationCode>> findByUserId(Long userId);
 
-    @Query("update ActivationCode a set a.isDeleted=1 where a.user.id=:#{#userId}")
+    @Transactional
+    @Modifying
+    //@Query("update ActivationCode a set a.isDeleted=1 where a.user.id=:#{#userId}")
     void expirationCode(Long userId);
 
 }
