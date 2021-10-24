dcb41808275e65fa209e4d33dde3aca54ec4594a
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-10-24 15:58:07 +0330

    Update MultimediaRepository.java

--- workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/MultimediaRepository.java
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/MultimediaRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MultimediaRepository extends BaseRepository<Multimedia, Long> {
 
-    //@Query("Select m from Multimedia m where m.user.id = :#{#user.id} and m.fileName = :#{#fileName}")
+    @Query("Select m from Multimedia m where m.user.id = :#{#user.id} and m.fileName = :#{#fileName}")
     Multimedia findByUserAndFileName(User user, String fileName);
 
 }
