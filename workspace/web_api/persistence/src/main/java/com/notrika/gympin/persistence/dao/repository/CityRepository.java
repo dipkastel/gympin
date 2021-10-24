d60b6f49104517152c9b564c090be15f9c88c0e3
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2021-10-24 15:57:48 +0330

    Update CityRepository.java

--- workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/CityRepository.java
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/CityRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Repository
 public interface CityRepository extends BaseRepository<City, Long> {
 
-    //@Query("select c from City c where c.deleted = false and c.state.id = :#{#state.id}")
+    @Query("select c from City c where c.deleted = false and c.state.id = :#{#state.id}")
     List<City> getCitiesByState(@Param("state") State state);
 
 }
