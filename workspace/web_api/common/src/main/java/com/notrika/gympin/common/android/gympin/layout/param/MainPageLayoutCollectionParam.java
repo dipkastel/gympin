b6f5f820f4a0744a96eaba9085cf1394c8662452
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2022-03-24 13:59:02 +0430

    [feature] add main page layout for app

--- workspace/web_api/common/src/main/java/com/notrika/gympin/common/android/gympin/layout/param/MainPageLayoutCollectionParam.java
+++ workspace/web_api/common/src/main/java/com/notrika/gympin/common/android/gympin/layout/param/MainPageLayoutCollectionParam.java
@@ -18,6 +18,6 @@ public class MainPageLayoutCollectionParam extends BaseParam<MainPageLayoutColle
     @JsonProperty("CollectionName")
     private String collectionName;
 
-    @JsonProperty("LayoutItem")
+    @JsonProperty("LayoutItems")
     private List<MainPageLayoutItemParam> layoutItems;
 }
