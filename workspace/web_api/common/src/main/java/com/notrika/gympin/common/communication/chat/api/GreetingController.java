5b985174eb4d748fd153a4b3f4db92edf1cc15be
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2022-06-04 20:08:23 +0430

    [feature] chat

--- workspace/web_api/common/src/main/java/com/notrika/gympin/common/communication/chat/api/GreetingController.java
+++ workspace/web_api/common/src/main/java/com/notrika/gympin/common/communication/chat/api/GreetingController.java
@@ -2,8 +2,12 @@ package com.notrika.gympin.common.communication.chat.api;
 
 import com.notrika.gympin.common.communication.chat.dto.GreetingDto;
 import com.notrika.gympin.common.communication.chat.param.GreetingParam;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 
 public interface GreetingController {
-    GreetingDto greeting(String driverId, GreetingParam message);
+//    GreetingDto greeting(String driverId, GreetingParam message);
+GreetingDto greeting(Message messagea, MessageHeaders messageHeaders, SimpMessageHeaderAccessor accessor, java.security.Principal principal, String driverId, GreetingParam message);
 }
--- workspace/web_api/controllerImpl/src/main/java/com/notrika/gympin/controller/impl/communication/chat/GreetingControllerImpl.java
+++ workspace/web_api/controllerImpl/src/main/java/com/notrika/gympin/controller/impl/communication/chat/GreetingControllerImpl.java
@@ -6,8 +6,11 @@ import com.notrika.gympin.common.communication.chat.dto.GreetingDto;
 import com.notrika.gympin.common.communication.chat.param.GreetingParam;
 import com.notrika.gympin.common.communication.chat.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,7 +25,10 @@ public class GreetingControllerImpl implements GreetingController {
     @SendTo("/chat/{driverId}/greetings")
     @IgnoreWrapAspect
     @Override
-    public GreetingDto greeting(String driverId, GreetingParam message) {
-        return GreetingDto.builder().foo(message.getFoo()).bar(message.getBar()).baz(message.getBaz()).build();
-    }
+    public GreetingDto greeting(Message messagea, MessageHeaders messageHeaders, SimpMessageHeaderAccessor accessor, java.security.Principal principal, String driverId, GreetingParam message) {
+                return GreetingDto.builder().foo(message.getFoo()).bar(message.getBar()).baz(message.getBaz()).build();
+            }
+//    public GreetingDto greeting(String driverId, GreetingParam message) {
+//        return GreetingDto.builder().foo(message.getFoo()).bar(message.getBar()).baz(message.getBaz()).build();
+//    }
 }
