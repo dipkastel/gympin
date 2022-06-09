8f6eda0245a195af582e33affee15404c6eb0ae4
Sia-Namir <38693802+Sia-Namir@users.noreply.github.com>
2022-06-10 00:28:58 +0430

    [feature] getRateableUsers

--- workspace/web_api/controllerImpl/src/main/java/com/notrika/gympin/controller/impl/user/UserRateControllerImpl.java
+++ workspace/web_api/controllerImpl/src/main/java/com/notrika/gympin/controller/impl/user/UserRateControllerImpl.java
@@ -8,6 +8,7 @@ import com.notrika.gympin.common.user.param.UserRateParam;
 import com.notrika.gympin.common.user.service.UserRateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,6 +47,7 @@ public class UserRateControllerImpl implements UserRateController {
     }
 
     @Override
+    @GetMapping("/getRateableUsers")
     public ResponseEntity<List<RateableUsersDto>> getRateableUsers() {
         return ResponseEntity.ok(userRateService.getRateableUsers());
     }
