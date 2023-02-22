a487f9f0460166d7a6e25d971b6aca5892724dc0
amir <dipkastel@gmail.com>
2023-02-23 00:48:32 +0330

    fix build issue web_app

--- workspace/web_app/src/network/setupAxios.js
+++ workspace/web_app/src/network/setupAxios.js
@@ -1,7 +1,6 @@
 import {AuthApi} from "./api/NETWORKCONSTS";
 import {refreshToken} from "./api/account.api";
-import {useSelector} from "react-redux";
-import {authActions} from "../helper/redux/actions/AuthActions";
+import {authActions} from "/src/helper/redux/actions/AuthActions";
 
 
 export function setupAxios(axios, store) {
