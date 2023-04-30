0d13d6c27b4756a7fa7819c3e6e0579ec3b1dc7c
amir <dipkastel@gmail.com>
2023-04-30 22:07:22 +0330

    fix front nextjs app

--- workspace/web_front/pages/corporate.js
+++ workspace/web_front/pages/corporate.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import HomeTwoAbout from '../components/HomeTwo/HomeTwoAbout'
-import HomeTwoBrand from '../components/HomeTwo/HomeTwoBrand'
-import HomeTwoCounter from '../components/HomeTwo/HomeTwoCounter'
 import HomeTwoFeature from '../components/HomeTwo/HomeTwoFeature'
 import HomeTwoHero from '../components/HomeTwo/HomeTwoHero'
-import HomeTwoProject from '../components/HomeTwo/HomeTwoProject'
-import HomeTwoService from '../components/HomeTwo/HomeTwoService'
-import HomeTwoTeam from '../components/HomeTwo/HomeTwoTeam'
-import HomeTwoTestimonial from '../components/HomeTwo/HomeTwoTestimonial'
 import HomeTwoVideo from '../components/HomeTwo/HomeTwoVideo'
-import HomeTwoWCU from '../components/HomeTwo/HomeTwoWCU'
-import HomeTwoWorkingProcess from '../components/HomeTwo/HomeTwoWorkingProcess'
-import HomeTwoNews  from '../components/HomeTwo/HomeTwoNews'
 import Layout from '../layouts/Layout'
 
 export default function corporate() {
