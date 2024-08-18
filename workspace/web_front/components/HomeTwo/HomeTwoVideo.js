f043d4d01ecc2c874d935f9644bd78134a8f36ae
amir <dipkastel@gmail.com>
2024-08-18 12:58:51 +0330

    deploy v2.6.11 front ->  fix send corporate message

--- workspace/web_front/components/HomeTwo/HomeTwoVideo.js
+++ workspace/web_front/components/HomeTwo/HomeTwoVideo.js
@@ -1,46 +1,55 @@
-import React, {useCallback, useEffect, useRef, useState} from 'react'
-import VideoPopUp from '../Plugins/VideoPopup'
-import {GoogleReCaptcha, useGoogleReCaptcha} from 'react-google-recaptcha-v3';
+import React, {useCallback, useRef, useState} from 'react'
+import {useGoogleReCaptcha} from 'react-google-recaptcha-v3';
 import getBaseUrl from "../../pages/api/network";
 
+
 export default function HomeTwoVideo() {
-    const { executeRecaptcha } = useGoogleReCaptcha();
-    const [loading,setLoading] = useState(false)
+
+    const pageStates = {CL: 0, LO: 1, TX: 2}
+    const {executeRecaptcha} = useGoogleReCaptcha();
+    const [formStatus, setFormStatus] = useState(pageStates.CL);
     const video = useRef(null)
     const videoPlaceHolder = useRef(null)
 
-    const handleReCaptchaVerify= useCallback(async (e) => {
+    const handleReCaptchaVerify = useCallback(async (e) => {
         if (!executeRecaptcha) {
             console.log('Execute recaptcha not yet available');
             return;
         }
         const token = await executeRecaptcha();
-        handleVerify(token,e)
+        handleVerify(token, e)
     }, [executeRecaptcha]);
 
     function submitForm(value) {
         value.preventDefault();
+        setFormStatus(pageStates.LO);
         handleReCaptchaVerify(value);
         // console.log("form value:", value);
     }
-    function handleVerify(value,e) {
-        if(value){
+
+    function handleVerify(value, e) {
+        if (value) {
             requestRegisterAdvise(e)
+        }else{
+            setFormStatus(pageStates.CL);
         }
     }
 
 
     const requestRegisterAdvise = async (e) => {
-        if(!e.target.Name.value){
+        if (!e.target.Name.value) {
             alert("نام نباید خالی باشد");
+            setFormStatus(pageStates.CL);
             return;
         }
-        if(!e.target.Tel.value){
+        if (!e.target.Tel.value) {
             alert("تلفن نباید خالی باشد");
+            setFormStatus(pageStates.CL);
             return;
         }
-        if(!e.target.Message.value){
+        if (!e.target.Message.value) {
             alert("پیام نباید خالی باشد");
+            setFormStatus(pageStates.CL);
             return;
         }
         const requestOptions = {
@@ -48,30 +57,82 @@ export default function HomeTwoVideo() {
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
                 PhoneNumber: e.target.Tel.value,
-                fullName: e.target.Name.value,
-                placeName: e.target.Message.value
+                FullName: e.target.Name.value,
+                Text: e.target.Message.value
             })
         };
-        setLoading(true);
-        await fetch(getBaseUrl()+"/v1/account/requestRegisterAdvise", requestOptions)
+        await fetch(getBaseUrl() + "/v1/account/requestRegisterAdvise", requestOptions)
             .then(response => response.json())
             .then(result => {
-                setLoading(false);
-                alert("پیام شما ثبت شد به زودی با شما تماس خواهیم گرفت .")
+                setFormStatus(pageStates.TX);
                 e.target.Tel.value = "";
                 e.target.Name.value = "";
                 e.target.Message.value = "";
-            }).catch(e=>{
-                setLoading(false);
-                alert("خطا")
+            }).catch(e => {
+                setFormStatus(pageStates.CL);
             })
     }
 
     function playVideo(e) {
         console.log(video);
-         video.current.removeAttribute("hidden");
-        videoPlaceHolder.current.setAttribute("hidden",true);
+        video.current.removeAttribute("hidden");
+        videoPlaceHolder.current.setAttribute("hidden", true);
         video.current.play();
+    }
+
+    function renderFormClear() {
+        return (<>
+
+            <div className="quote-wrapper">
+                <h2 className="quote-title">مشاوره رایگان</h2>
+                <div className="quote-form rtl">
+                    <form onSubmit={submitForm} className="mt-none-30">
+                        <div className="form-group mt-10">
+                            <input type="text" name="Name" id="name" placeholder="نام"/>
+                        </div>
+                        <div className="form-group mt-10">
+                            <input type={"number"} name="Tel" id="tel" placeholder="تلفن"/>
+                        </div>
+                        <div className="form-group mt-10">
+                            <textarea name="Message" id="message" placeholder="پیام"/>
+                        </div>
+                        <div className="form-group mt-10">
+                            <button type="submit" className="quote-btn"> ارسال
+                                <span className="icon"><i
+                                    className="far fa-arrow-left"/></span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>);
+    }
+
+    function renderFormTnx() {
+        return (
+            <div className="quote-wrapper">
+                <h2 className="quote-title">همکاران ما به زودی با شما تماس خواهند گرفت </h2>
+                <h4 className="quote-form">با تشکر</h4>
+            </div>);
+    }
+
+    function renderFormLoding() {
+        return (<div className="quote-wrapper">
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>);
+    }
+
+    function renderFormState() {
+        switch (formStatus) {
+            case pageStates.CL:
+                return renderFormClear();
+            case pageStates.TX:
+                return renderFormTnx();
+            case pageStates.LO:
+                return renderFormLoding();
+        }
     }
 
     return (
@@ -82,7 +143,8 @@ export default function HomeTwoVideo() {
                     <div className="container-fluid">
                         <div className="row no-gutters">
                             <div className="col-xl-8 ">
-                                <video  ref={video} height={"100%"} width={"100%"} hidden={true} type="video/mp4" controls >
+                                <video ref={video} height={"100%"} width={"100%"} hidden={true} type="video/mp4"
+                                       controls>
                                     <source src="/videos/gympin-c-5.mp4" type="video/mp4"/>
                                     Your browser does not support the video tag.
                                 </video>
@@ -91,10 +153,10 @@ export default function HomeTwoVideo() {
                                      data-opacity="34">
                                     <div className="video-container-wrap">
                                         <div
-                                           data-rel="lightcase:myCollection" data-animation="fadeInLeft"
-                                           data-delay=".1s"
-                                           className="video-link pointer">
-                                            <div className="video-play-wrap" onClick={e=>playVideo(e)}>
+                                            data-rel="lightcase:myCollection" data-animation="fadeInLeft"
+                                            data-delay=".1s"
+                                            className="video-link pointer">
+                                            <div className="video-play-wrap" onClick={e => playVideo(e)}>
                                                 <div className="video-mark">
                                                     <div className="wave-pulse wave-pulse-1"></div>
                                                     <div className="wave-pulse wave-pulse-2"></div>
@@ -108,28 +170,7 @@ export default function HomeTwoVideo() {
                                 </div>
                             </div>
                             <div className="col-xl-4">
-                                <div className="quote-wrapper">
-                                    <h2 className="quote-title">مشاوره رایگان</h2>
-                                    <div className="quote-form rtl">
-                                        <form onSubmit={submitForm} className="mt-none-30">
-                                            <div className="form-group mt-10">
-                                                <input disabled={loading} type="text" name="Name" id="name" placeholder="نام"/>
-                                            </div>
-                                            <div className="form-group mt-10">
-                                                <input disabled={loading} type={"number"} name="Tel" id="tel" placeholder="تلفن"/>
-                                            </div>
-                                            <div className="form-group mt-10">
-                                                <textarea  disabled={loading} name="Message" id="message" placeholder="پیام"/>
-                                            </div>
-                                            <div className="form-group mt-10">
-                                                <button disabled={loading} type="submit" className="quote-btn"> ارسال
-                                                    <span className="icon"><i
-                                                        className="far fa-arrow-left"/></span>
-                                                </button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
+                                {renderFormState()}
                             </div>
                         </div>
                     </div>
