4d6e5e648b37cab1a7f946a8366135c07c788704
amir <dipkastel@gmail.com>
2024-08-20 21:12:46 +0330

    deploy v2.6.17 place ->  fix home helper

--- workspace/web_master/src/helper/serverSettingsHelper.js
+++ workspace/web_master/src/helper/serverSettingsHelper.js
@@ -1,8 +1,8 @@
 export function getHomeId(settings) {
     try {
-        var userSetting = settings?.settings?.server.UserSettings.find(s => s.Key === "PLACE_HOME_PAGE_ID").Value
+        var userSetting = settings?.settings?.server?.UserSettings?.find(s => s.Key === "PLACE_HOME_PAGE_ID")?.Value
         if(userSetting&&userSetting!="") return userSetting;
-        var publicSettings = settings.settings.server.Settings.find(s => s.Key === "WEB_MASTER_HOMEPAGE_ID").Value;
+        var publicSettings = settings?.settings?.server?.Settings?.find(s => s.Key === "WEB_MASTER_HOMEPAGE_ID")?.Value;
         if(publicSettings&&publicSettings!="") return publicSettings;
         return 2;
     } catch (e) {
