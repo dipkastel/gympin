3cec9fbf823c7f14e00f777b75c2ef69fb33daa9
amir <dipkastel@gmail.com>
2024-09-29 14:15:52 +0330

    deploy v2.9.2 front -> fix issue

--- workspace/web_front/pages/index.js
+++ workspace/web_front/pages/index.js
@@ -1,17 +1,7 @@
-import Image from 'next/image'
 import HomeOneAbout from '../components/HomeOne/HomeOneAbout'
-import HomeOneBrand from '../components/HomeOne/HomeOneBrand'
 import HomeOneCTA from '../components/HomeOne/HomeOneCTA'
-import HomeOneCTA2 from '../components/HomeOne/HomeOneCTA2'
 import HomeOneFAQ from '../components/HomeOne/HomeOneFAQ'
-import HomeOneFeature from '../components/HomeOne/HomeOneFeature'
-import HomeOneGTA from '../components/HomeOne/HomeOneGTA'
 import HomeOneHero from '../components/HomeOne/HomeOneHero'
-import HomeOneNews from '../components/HomeOne/HomeOneNews'
-import HomeOnePricing from '../components/HomeOne/HomeOnePricing'
-import HomeOneProject from '../components/HomeOne/HomeOneProject'
-import HomeOneService from '../components/HomeOne/HomeOneService'
-import HomeOneTestimonial from '../components/HomeOne/HomeOneTestimonial'
 import Layout from '../layouts/Layout'
 import HomeOneTestimonial2 from "../components/HomeOne/HomeOneTestimonial2";
 import HomeOneGoal from "../components/HomeOne/HomeOneGoal";
@@ -23,14 +13,14 @@ export default function Home() {
     return (
         <>
             <Layout>
-                <HomeOneHero />
-                <HomeOneTestimonial2 />
-                <HomeOneGoal />
-                <HomeOneDesc1 />
-                <HomeOneDesc2 />
-                <HomeOneCTA />
-                <HomeOneAbout />
-                <HomeOneApplications />
+                <HomeOneHero/>
+                <HomeOneTestimonial2/>
+                <HomeOneGoal/>
+                <HomeOneDesc1/>
+                <HomeOneDesc2/>
+                <HomeOneCTA/>
+                <HomeOneAbout/>
+                <HomeOneApplications/>
                 {/*<HomeOneFeature />*/}
                 {/*<HomeOneTestimonial />*/}
                 {/*<HomeOneService />*/}
@@ -39,7 +29,7 @@ export default function Home() {
                 {/*<HomeOneNews />*/}
                 {/*<HomeOneCTA2 />*/}
                 {/*<HomeOneGTA />*/}
-                <HomeOneFAQ />
+                <HomeOneFAQ/>
                 {/*<HomeOneBrand />*/}
             </Layout>
         </>
