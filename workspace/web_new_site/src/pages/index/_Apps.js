6f9e8d08d4703b13d6384547d7129521cab0c9c0
amir <dipkastel@gmail.com>
2025-01-06 21:32:20 +0330

    deploy v2.11.13 web-front -> change some ...

--- workspace/web_new_site/src/pages/index/_Apps.js
+++ workspace/web_new_site/src/pages/index/_Apps.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Grid from "@mui/material/Grid2";
 import {Container, Typography} from "@mui/material";
-import _TextImage from "../partials/_TextImage";
 
 const _Apps = () => {
     return (
-        <section >
+        <section>
             <Container>
                 <Grid container columns={4} alignContent={"center"} textAlign={"center"}>
                     <Grid item size={4}>
-                        <Typography color={"primary"} sx={{fontWeight: 600,  mb: 3}} variant={"h1"}>اپلیکیشن‌های اختصاصی</Typography>
+                        <Typography color={"primary"} sx={{fontWeight: 600, mb: 3}} variant={"h1"}>اپلیکیشن‌های اختصاصی</Typography>
                     </Grid>
                 </Grid>
                 <Grid container sx={{direction: "ltr"}} columns={4} alignContent={"center"} textAlign={"right"}>
@@ -18,18 +17,15 @@ const _Apps = () => {
                     </Grid>
                     <Grid sx={{px: 6, direction: "rtl"}} item size={{md: 2, xs: 4}}>
                         <Typography sx={{fontWeight: 600, mt: 8}} variant={"h5"}>پنل اختصاصی سازمان‌ها و شرکت‌ها</Typography>
-                        <Typography sx={{fontWeight: 400, mt: 2, mb: 0,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>با استفاده از قابلیت
-                           ‌های زیر، مدیران می‌توانند پرسنل خود را به طرز بهینه ای مدیریت کنند، از اتلاف هزینه‌ها جلوگیری کرده و با داشتن
-                            اطلاعات دقیق، تصمیمات بهتری برای سازمان خود بگیرند.
-                            </Typography>
-
-                        <Typography sx={{fontWeight: 400, mt: 0, mb: 3,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>مشاهده ی فعالیت‌های
-                            کارمندان بر روی پنل،
-                            رهگیری اعتبار‌های داده شده، دسته بندی کارمندان
-                            و اعتبار دهی فردی یا گروهی برای تشویق، کارت هدیه‌ی مخصوص مراکز ورزشی برای مناسبت‌ها (نوروز، یلدا، تولد و غیره)، کاهش اعتبار یا مسدود کردن دسترسی پرسنل، بازگشت اعتبارهای
-                            استفاده نشده به کیف پول شرکت و استفاده از آن برای اعتباردهی مجدد، پاسخگویی و
-                            پشتیانی کامل
-                            </Typography>
+                        <Typography sx={{fontWeight: 400, mt: 2, mb: 0, lineHeight: 2, textAlign: "justify"}} variant={"subtitle2"}>پنل
+                            اختصاصی سازمان‌ها و شرکت‌ها در جیم پین، ابزاری جامع برای مدیریت فعالیت‌های کارکنان و تخصیص اعتبار به آن‌ها است.
+                            این پنل امکان دسته‌بندی پرسنل و ارائه مشوق‌های مالی را فراهم می‌کند. همچنین، مدیران می‌توانند این اعتبارات را کاهش
+                            دهند یا دسترسی کارکنان را مسدود سازند.
+                        </Typography>
+                        <Typography sx={{fontWeight: 400, mt: 0, mb: 0, lineHeight: 2, textAlign: "justify"}} variant={"subtitle2"}> اعتبارات
+                            استفاده ‌نشده از بین نمی‌رود و قابل استفاده مجدد می‌باشد. به‌طور کلی، این پنل به بهینه‌سازی مدیریت منابع انسانی و کنترل هزینه‌ها
+                            کمک می‌کند.
+                        </Typography>
 
                         {/*<Typography sx={{fontWeight: 400, mt: 0, mb: 0,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>*/}
                         {/*    <ul>*/}
@@ -47,12 +43,13 @@ const _Apps = () => {
                         <img alt={"اپلیکیشن اختصاصی شرکت‌ها"} className={"apps-img"} src={"/assets/images/appuser.jpg"}/>
                     </Grid>
                     <Grid sx={{px: 6}} item size={{md: 2, xs: 4}}>
-                        <Typography sx={{fontWeight: 600,lineHeight:2, mt: 8}} variant={"h5"}>اپلیکیشن اختصاصی کارمندان</Typography>
-                        <Typography sx={{fontWeight: 400, mt: 2, mb: 3,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>اپلیکیشن کارمندان یک
+                        <Typography sx={{fontWeight: 600, lineHeight: 2, mt: 8}} variant={"h5"}>اپلیکیشن اختصاصی کارمندان</Typography>
+                        <Typography sx={{fontWeight: 400, mt: 2, mb: 3, lineHeight: 2, textAlign: "justify"}} variant={"subtitle2"}>اپلیکیشن
+                            کارمندان یک
                             ابزار کارآمد و چند منظوره است که در آن کارمندان از طریق حساب کاربری خود می‌توانند تمامی‌اطلاعات لازم درباره ی
                             مجموعه‌های ورزشی و امکانات آنها را کسب کنند. این اپلیکیشن دارای امکاناتی مانند:
                             مشاهده‌ی عکس‌ها و امکانات مجموعه‌های ورزشی طرف قرارداد، مشاهده ی رشته ی‌های ورزشی هر مجموعه و روز و ساعت برگزاری
-                            هر کلاس، مشاهده ی آدرس مراکز ورزشی بر روی نقشه، امکان جستجوی نزدیک ترین مراکز ورزشی، امکان رزرو کلاس‌ها هماهنگی
+                            هر کلاس، مشاهده ی آدرس مراکز ورزشی بر روی نقشه، امکان جستجوی نزدیک ترین مراکز ورزشی و مسیریابی، امکان رزرو کلاس‌ها هماهنگی
                             قبل از خرید، پاسخگویی و پشتیانی کامل</Typography>
                     </Grid>
                 </Grid>
@@ -63,11 +60,13 @@ const _Apps = () => {
                     </Grid>
                     <Grid sx={{px: 6, direction: "rtl"}} item size={{md: 2, xs: 4}}>
                         <Typography sx={{fontWeight: 600, mt: 8}} variant={"h5"}>پنل اختصاصی مراکز طرف قرارداد</Typography>
-                        <Typography sx={{fontWeight: 400, mt: 2, mb: 1,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>پنل اختصاصی مراکز ورزشی
+                        <Typography sx={{fontWeight: 400, mt: 2, mb: 1, lineHeight: 2, textAlign: "justify"}} variant={"subtitle2"}>پنل
+                            اختصاصی مراکز ورزشی
                             جیم پین دارای امکاناتی مانند: به‌روز رسانی قیمت‌ها، ویرایش بلیط‌ها، به‌روز رسانی اطلاعات مجموعه و اعلام تغییرات
                             است. همچنین بخش مالی شفاف و آسان، امکان اسکن بلیط، مشاهده بلیط‌های فروخته شده از دیگر امکانات این اپلیکیشن می
                             باشد.</Typography>
-                        <Typography sx={{fontWeight: 400, mt: 1, mb: 3,lineHeight:2, textAlign: "justify"}} variant={"subtitle2"}>مراکز ورزشی از طریق
+                        <Typography sx={{fontWeight: 400, mt: 0, mb: 3, lineHeight: 2, textAlign: "justify"}} variant={"subtitle2"}>مراکز
+                            ورزشی از طریق
                             اسکن بلیط کاربر، ورود کاربران را ثبت کرده و بقیه موارد از طریق جیم پین انجام می‌شود.</Typography>
                     </Grid>
                 </Grid>
--- workspace/web_new_site/src/pages/index/_Sports.js
+++ workspace/web_new_site/src/pages/index/_Sports.js
@@ -3,9 +3,9 @@ import Grid from "@mui/material/Grid2";
 import {Card, CardContent, CardHeader, Container, Typography} from "@mui/material";
 
 const _Sports = () => {
-    function SportItem({title,alt,image,text}) {
+    function SportItem({title,alt,image,text,size}) {
         return (
-            <Grid size={{xs: 6, sm: 6, md: 3,xl:2}} justifyItems={"center"}>
+            <Grid size={size} justifyItems={"center"}>
                 <Card className={"sportCard"} elevation={14} sx={{backgroundImage:image}} >
                     <img className={"bgImage"} alt={alt} src={image}/>
                     <div className={"overlay"}>
@@ -30,12 +30,15 @@ const _Sports = () => {
         <section>
             <Container>
                 <Grid sx={{mb: 8,mt:12}} container columns={6}  >
-                    <SportItem image={"/assets/images/estakhr.jpg"} alt={"ورزش‌های آبی که برای کارمندان ایجاد نشاط می‌کند"} title={"ورزش‌های آبی"} text={"قدرت و شادابی را در دل آب پیدا کنید! با ورزش‌های آبی، تغییرات مثبت را در بدن و ذهن خود تجربه کنید و به بهترین نسخه از خود تبدیل شوید."}/>
-                    <SportItem image={"/assets/images/bodybuilding.jpg"} alt={"ورزش‌هایی که برای عموم مردم جذاب است"} title={"باشگاه‌های بدنسازی"} text={"هر بار که به باشگاه می‌آیید، یک قدم به هدف‌های خود نزدیک‌تر می‌شوید! با جیم پین، هر تمرین فرصتی برای تغییر و تحول است."}/>
-                    <SportItem image={"/assets/images/shamshir.jpg"} alt={"پدل برد ورزش مورد علاقه مدیران"} title={"ورزش‌های رزمی"} text={"آماده‌اید تا مرزهای خود را بشکنید؟ با ورزش‌های رزمی جیم پین، هر جلسه تمرین فرصتی برای ارتقاء مهارت‌ها و تقویت روحیه‌تان است."}/>
-                    <SportItem image={"/assets/images/zorkhane.jpg"} alt={"از زورخانه تا پارکور، آرامش تا هیجان"} title={"ورزش‌های برگزیده"} text={"هر ورزشی که انتخاب کنید، فرصتی برای کشف قدرت‌های نهفته شماست! بیایید با هم به سمت موفقیت‌های بزرگ‌تر حرکت کنیم."}/>
-                    <SportItem image={"/assets/images/padel.jpg"} alt={"تفریحاتی برای ایجاد انرژی و انگیزه"} title={"ورزش‌های تفریحی"} text={"ورزش را به یک ماجراجویی تبدیل کنید! با فعالیت‌های تفریحی، استرس را کنار بگذارید و از هر لحظه لذت ببرید."}/>
-                    <SportItem image={"/assets/images/spa.jpg"} alt={"آرامش و حس خوب برای کارمندان"} title={"ماساژ و اسپا"} text={"با خدمات اسپا و ماساژ، به بدن و ذهن خود یک هدیه ویژه بدهید! احساس آرامش و تازگی را با هر لمس تجربه کنید."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/estakhr.jpg"} alt={"ورزش‌های آبی که برای کارمندان ایجاد نشاط می‌کند"} title={"ورزش‌های آبی"} text={"قدرت و شادابی را در دل آب پیدا کنید! با ورزش‌های آبی، تغییرات مثبت را در بدن و ذهن خود تجربه کنید و به بهترین نسخه از خود تبدیل شوید."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/bodybuilding.jpg"} alt={"ورزش‌هایی که برای عموم مردم جذاب است"} title={"باشگاه‌های بدنسازی"} text={"هر بار که به باشگاه می‌آیید، یک قدم به هدف‌های خود نزدیک‌تر می‌شوید! با جیم پین، هر تمرین فرصتی برای تغییر و تحول است."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/shamshir.jpg"} alt={"پدل برد ورزش مورد علاقه مدیران"} title={"ورزش‌های رزمی"} text={"آماده‌اید تا مرزهای خود را بشکنید؟ با ورزش‌های رزمی جیم پین، هر جلسه تمرین فرصتی برای ارتقاء مهارت‌ها و تقویت روحیه‌تان است."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/zorkhane.jpg"} alt={"از زورخانه تا پارکور، آرامش تا هیجان"} title={"ورزش‌های برگزیده"} text={"هر ورزشی که انتخاب کنید، فرصتی برای کشف قدرت‌های نهفته شماست! بیایید با هم به سمت موفقیت‌های بزرگ‌تر حرکت کنیم."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/padel.jpg"} alt={"تفریحاتی برای ایجاد انرژی و انگیزه"} title={"ورزش‌های تفریحی"} text={"ورزش را به یک ماجراجویی تبدیل کنید! با فعالیت‌های تفریحی، استرس را کنار بگذارید و از هر لحظه لذت ببرید."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/spa.jpg"} alt={"آرامش و حس خوب برای کارمندان"} title={"ماساژ و اسپا"} text={"با خدمات اسپا و ماساژ، به بدن و ذهن خود یک هدیه ویژه بدهید! احساس آرامش و تازگی را با هر لمس تجربه کنید."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/analiz-badan.jpg"} alt={"آنالیز بدن "} title={"آنالیز بدن"} text={"از بدن خود اطلاعات بیشتری به دست بیاورید و با ورزش مناسب از بیماری ها جلوگیری کنید."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/harakate-eslahi.jpg"} alt={"حرکات اصلاحی "} title={"حرکات اصلاحی"} text={"عموما کارمندان پشت میز نشین دچار گرفتگی های عضلانی و بیماری های مفصلی می شوند که بهترین درمان برای آنان حرکات اصلاحی است."}/>
+                    <SportItem size={{xs: 3, sm: 3, md: 2,xl:2}} image={"/assets/images/toopi.jpg"} alt={"ورزش‌های گروهی و تیمی"} title={"ورزش های کروهی"} text={"با ورزش های گروهی احساس تعامل و همکاری در گروه ها بیشتر شده و در کار های گروهی موفقیت بیشتری به دست می آورند."}/>
                 </Grid>
             </Container>
         </section>
