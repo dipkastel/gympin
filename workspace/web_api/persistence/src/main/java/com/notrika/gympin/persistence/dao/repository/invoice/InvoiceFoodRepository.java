60342f5e5f9fc4a62ffed01a424ab5ab3fda86a6
amir <dipkastel@gmail.com>
2025-07-29 21:19:38 +0330

    deploy v3.07.10 api -> and more about food and gyms

---
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/invoice/InvoiceFoodRepository.java
@@ -0,0 +1,13 @@
+package com.notrika.gympin.persistence.dao.repository.invoice;
+
+import com.notrika.gympin.persistence.dao.repository.BaseRepository;
+import com.notrika.gympin.persistence.entity.finance.user.invoice.InvoiceBuyableEntity;
+import com.notrika.gympin.persistence.entity.finance.user.invoice.InvoiceEntity;
+import com.notrika.gympin.persistence.entity.finance.user.invoice.InvoiceFoodEntity;
+
+import java.util.List;
+
+public interface InvoiceFoodRepository extends BaseRepository<InvoiceFoodEntity, Long> {
+
+
+}
---
+++ workspace/web_api/persistence/src/main/java/com/notrika/gympin/persistence/dao/repository/invoice/InvoiceSubscribeRepository.java
@@ -0,0 +1,10 @@
+package com.notrika.gympin.persistence.dao.repository.invoice;
+
+import com.notrika.gympin.persistence.dao.repository.BaseRepository;
+import com.notrika.gympin.persistence.entity.finance.user.invoice.InvoiceFoodEntity;
+import com.notrika.gympin.persistence.entity.finance.user.invoice.InvoiceSubscribeEntity;
+
+public interface InvoiceSubscribeRepository extends BaseRepository<InvoiceSubscribeEntity, Long> {
+
+
+}
